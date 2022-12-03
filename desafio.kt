2a47717069441511825a5826ccb21b37956e5b4b
Edson Santos <33705500+edson-ricardo@users.noreply.github.com>
2022-12-03 17:16:46 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,92 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+/*
+ Sistema modelado de um aluno do curso.
+ Confesso que tentei fazer, mas não ficou muito legal.
+ Modelei este abaixo e estudei cada código para entender o que estava acontecendo.
+*/
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val CPF: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, var nivel: Nivel, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+enum class Nivel { Basico, Intermediario, Avancado }
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+
+    fun listaDeContatos() {
+        for (usuario in inscritos) {
+            println("Nome: ${usuario.nome} | CPF: ${usuario.CPF}")
+        }
+    }
+
+    /*fun conteudoFormacao() {
+        for (conteudo in conteudos) {
+            println("${conteudo.nome} - ${conteudo.duracao}h")
+        }
+    }*/
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val financasConteudo = ConteudoEducacional("Finanças Pessoais", Nivel.Basico, 80)
+    val inglesConteudo = ConteudoEducacional("Inglês", Nivel.Intermediario, 120)
+    val KotlinConteudo = ConteudoEducacional("Desenvolvimento Kotlin", Nivel.Avancado, 100)
+    val listaDeConteudo = mutableListOf<ConteudoEducacional>()
+    listaDeConteudo.add(financasConteudo)
+    listaDeConteudo.add(inglesConteudo)
+    listaDeConteudo.add(KotlinConteudo)
+
+    val financasFormacao = Formacao("Finanças Pessoais", listaDeConteudo)
+    val inglesFormacao = Formacao("Inglês", listaDeConteudo)
+    val KotlinFormacao = Formacao("Desenvolvimento Kotlin", listaDeConteudo)
+
+    val usuario1 = Usuario("João", "087.387.987-87")
+    val usuario2 = Usuario("Maria", "005.067.087-85")
+    val usuario3 = Usuario("José", "040.040.036-20")
+    val usuario4 = Usuario("Lúcia", "080.080.056-25")
+    val usuario5 = Usuario("Jaime", "036.035.044-30")
+    val usuario6 = Usuario("Josefa", "036.035.044-30")
+
+    financasFormacao.matricular(usuario1)
+    financasFormacao.matricular(usuario2)
+    financasFormacao.matricular(usuario3)
+    financasFormacao.matricular(usuario4)
+
+    KotlinFormacao.matricular(usuario4)
+    KotlinFormacao.matricular(usuario5)
+    KotlinFormacao.matricular(usuario6)
+
+    inglesFormacao.matricular(usuario1)
+    inglesFormacao.matricular(usuario4)
+    inglesFormacao.matricular(usuario6)
+
+    println("DADOS DA FORMAÇÃO")
+    println("------------------------------------------------------------------------------------")
+    println("FORMAÇÕES:")
+    for (conteudo in listaDeConteudo) {
+        println("${conteudo.nome} - ${conteudo.duracao}h")
+    }
+    println("------------------------------------------------------------------------------------")
+
+    println("Curso: ${financasConteudo.nome} | Nível: ${financasConteudo.nivel} |" +
+            " Duração: ${financasConteudo.duracao}h")
+    println("Aluno(s) cadastrado(s):")
+    println("Nome: ${financasFormacao.listaDeContatos()}")
+    println("------------------------------------------------------------------------------------")
+
+    println("Curso: ${inglesConteudo.nome} | Nível: ${inglesConteudo.nivel} | Duração: ${inglesConteudo.duracao}h")
+    println("Aluno(s) cadastrado(s):")
+    println("Nome: ${inglesFormacao.listaDeContatos()}")
+    println("------------------------------------------------------------------------------------")
+
+    println("Curso: ${KotlinConteudo.nome} | Nível: ${KotlinConteudo.nivel} | Duração: ${KotlinConteudo.duracao}h")
+    println("Aluno(s) cadastrado(s):")
+    println("Nome: ${KotlinFormacao.listaDeContatos()}")
+    println("------------------------------------------------------------------------------------")
+
 }
